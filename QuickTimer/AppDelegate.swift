2e28bfe20a5be708804fab2cd7e4089817d7f8fa
Inder Dhir <inderdhir@hotmail.com>
2017-05-24 22:59:32 -0400

    Initial Commit

---
+++ QuickTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QuickTimer
+//
+//  Created by Inder Dhir on 5/24/17.
+//  Copyright © 2017 Inder Dhir. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
